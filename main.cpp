56e855db0cabdd56911c246ed11c12c42cd154b7
VillifCoder559 <francesco.villi@gmail.com>
2022-11-17 20:02:15 +0100

    add read_points from external file

--- main.cpp
+++ main.cpp
@@ -1,45 +1,50 @@
+#include <omp.h>
+
 #include <fstream>
 #include <iostream>
 
 #include "MeanShift.h"
 #include "Point.h"
-std::vector<Point> read_point_from_file();
+std::vector<Point> read_point_from_file(int);
 void print_all_points(std::vector<Point>);
-// bool approximatelyEqual(float a, float b, float epsilon)
-// {
-//     return fabs(a - b) <= ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
-// }
+
 int main() {
-	std::vector<Point> points=read_point_from_file();
-	MeanShift clustering=MeanShift(points,1);
-	auto new_points=clustering.fit();
-	std::cout<<"NEW_POINTS"<<std::endl;
-	print_all_points(new_points);
+  std::vector<Point> points = read_point_from_file(2);
+  MeanShift clustering = MeanShift(points, 5, flat);
+  std::cout << "Bandwidth: " << clustering.bandwidth << " using Kernel: "
+            << (clustering.get_type_kernel() == gaussian ? "Gaussian" : "Flat")
+            << std::endl;
+  double start = omp_get_wtime();
+  auto new_points = clustering.fit();
+  double end = omp_get_wtime();
+  std::cout << "TIME: " << end - start << std::endl;
+  std::cout << "NEW_POINTS" << std::endl;
+  print_all_points(new_points);
   return 0;
 }
-std::vector<Point> read_point_from_file(){
-	std::vector<Point> points;
+std::vector<Point> read_point_from_file(int dim) {
+  std::vector<Point> points;
   std::ifstream file("..\\points.txt");
   std::string line;
-  int i = 0;
-  int count = 1;
-	points.push_back(Point());
+  int i = -1;
+  int count = 0;
+  // points.push_back(Point(3));
   while (std::getline(file, line, ',')) {
-		float value=std::stof(line);
-    points[i].add_coord(value);
-    if (count % 3 == 0) {
+    if (count % dim == 0) {
       i += 1;
-      points.push_back(Point());
+      points.push_back(Point(dim));
+      count=0;
     }
+    float value = std::stof(line);
+    points[i].add_coord(count,value);
     count++;
   }
-	points.pop_back();
   file.close();
-	return points;
+  return points;
 }
 
-void print_all_points(std::vector<Point> vector){
-	for(int i=0;i<vector.size();i++){
-		vector[i].print();
-	}
+void print_all_points(std::vector<Point> vector) {
+  for (int i = 0; i < vector.size(); i++) {
+    vector[i].print();
+  }
 }
