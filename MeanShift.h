063320ae9617e34a06090df21642c11c8dfbcd99
VillifCoder559 <francesco.villi@gmail.com>
2022-11-18 00:28:17 +0100

    add fun predict

--- MeanShift.h
+++ MeanShift.h
@@ -14,17 +14,19 @@ enum type_kernel {flat=0,gaussian=1};
 
 class MeanShift {
  public:
-  std::vector<Point> points;
+  std::vector<Point> centroids;
   float bandwidth;
   Kernel *kernel;
-  MeanShift(std::vector<Point> points, float bandwidth,type_kernel kernel);
+  MeanShift(float bandwidth,type_kernel kernel);
   ~MeanShift();
-  std::vector<Point> fit();
+  std::vector<Point> fit(std::vector<Point> points);
+  int predict(Point p);
   type_kernel get_type_kernel();
+  
  private:
-  Point compute_new_mean(Point p);
+  Point compute_new_mean(std::vector<Point> &points,Point p);
   // float compute_norm_L2(Point x1,Point x2);
-  std::vector<Point> get_centroids();
+  std::vector<Point> get_centroids(std::vector<Point> &points);
   std::vector<float> find_nearest_centroid(Point &point,std::vector<Point> &centroids);
 };
 #endif
