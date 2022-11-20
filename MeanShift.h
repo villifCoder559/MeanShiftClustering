47bd128bc53199a3c97c911ac9115dbe55dbdaab
VillifCoder559 <francesco.villi@gmail.com>
2022-11-20 09:51:55 +0100

    add association labels

--- MeanShift.h
+++ MeanShift.h
@@ -1,32 +1,41 @@
 #ifndef _MeanShift_
 #define _MeanShift_
-#include <math.h>
 #include "Kernel.h"
-#include "KernelGaussian.h"
 #include "KernelFlat.h"
-#include <map>
-#include <vector>
+#include "KernelGaussian.h"
 #include "Point.h"
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <vector>
+#include <omp.h>
 
-enum type_kernel {flat=0,gaussian=1};
+enum type_kernel { flat = 0, gaussian = 1 };
 
 class MeanShift {
- public:
-  std::vector<Point> centroids;
-  float bandwidth;
-  Kernel *kernel;
-  MeanShift(float bandwidth,type_kernel kernel);
+public:
+  MeanShift(float bandwidth, type_kernel kernel, short int max_iterations = 50);
   ~MeanShift();
   std::vector<Point> fit(std::vector<Point> points);
-  int predict(Point p);
+  // std::vector<Point> fit2(std::vector<Point> points);
+  /*Find the closest cluster to point p*/
+  unsigned short int predict(Point p);
   type_kernel get_type_kernel();
-  
- private:
-  Point compute_new_mean(std::vector<Point> &points,Point p);
-  // float compute_norm_L2(Point x1,Point x2);
-  std::vector<Point> get_centroids(std::vector<Point> &points);
-  std::vector<float> find_nearest_centroid(Point &point,std::vector<Point> &centroids);
+  /*id n-th corresponds to centroid in position n-th*/
+  std::vector<unsigned short int> get_labels();
+  float get_bandwidth();
+  // void compute_all_distances(std::vector<Point>& vector,int length,std::vector<float> &distances);
+
+private:
+  std::vector<Point> centroids;
+  std::vector<unsigned short int> labels;
+  float bandwidth;
+  Kernel *kernel;
+  short int max_iterations;
+  Point compute_new_mean(std::vector<Point> &points, Point p);
+  // Point compute_new_mean(std::vector<Point> &points, float j, std::vector<float> &result);
+  std::vector<Point> compute_centroids(std::vector<Point> &points);
+  std::vector<float> find_nearest_centroid(Point &point, std::vector<Point> &centroids);
 };
 #endif
