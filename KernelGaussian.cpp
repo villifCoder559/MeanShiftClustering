c9d52e8818d3120a4ea17f931514d16544611337
VillifCoder559 <francesco.villi@gmail.com>
2022-11-20 09:49:53 +0100

    reduce small values to zero

--- KernelGaussian.cpp
+++ KernelGaussian.cpp
@@ -1,7 +1,8 @@
 #include "KernelGaussian.h"
 
-KernelGaussian::KernelGaussian(){}
-KernelGaussian::~KernelGaussian(){}
+KernelGaussian::KernelGaussian() {}
+KernelGaussian::~KernelGaussian() {}
 float KernelGaussian::compute(float distance, float bandwith) {
-  return exp((-0.5 * pow(distance, 2) / (pow(bandwith, 2))));
+  float value = exp((-0.5 * pow(distance, 2) / (pow(bandwith, 2))));
+  return value > 0.00001 ? value : 0;
 }
