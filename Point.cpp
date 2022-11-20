f4f77cc388ae2f68ca55af9840781fac8525931c
VillifCoder559 <francesco.villi@gmail.com>
2022-11-20 09:54:48 +0100

    optimize calc L2 norm

--- Point.cpp
+++ Point.cpp
@@ -1,32 +1,40 @@
 #include "Point.h"
 Point::Point(int dim) {
-  // coords.reserve(dim);
-  for (int i = 0; i < dim; i++) coords.push_back(0.0);
+  coords.reserve(dim);
+  for (int i = 0; i < dim; i++)
+    coords.push_back(0.0);
 }
 Point::Point(std::vector<float> point) {
   coords.reserve(point.size());
-  for (int i = 0; i < point.size(); i++) coords.push_back(point[i]);
+  for (int i = 0; i < point.size(); i++)
+    coords.push_back(point[i]);
 }
 void Point::print() {
   std::cout << "[";
   for (int i = 0; i < coords.size(); i++) {
     std::cout << coords[i];
-    if (i != coords.size() - 1) std::cout << ", ";
+    if (i != coords.size() - 1)
+      std::cout << ", ";
   }
   std::cout << "]";
   std::cout << "," << std::endl;
 }
 
 void Point::add_coord(int pos, float value) {
-  if (pos < coords.size()) this->coords[pos] = value;
+  if (pos < coords.size())
+    this->coords[pos] = value;
 }
 
-float Point::eucledian_distance(Point &x1, Point &x2) {
+float Point::calc_L2_norm_approx(Point &x1, Point &x2) {
   float distance = 0;
   if (x1.coords.size() == x2.coords.size()) {
-    for (int i = 0; i < x1.coords.size(); i++)
-      distance += pow((x1.coords[i] - x2.coords[i]), 2);
-    return sqrt(distance);
+    const int dim=x1.coords.size();
+    for (int i = 0; i < dim; i++){
+    const float d=x1.coords[i]-x2.coords[i];
+    distance += d*d;
+    //  distance+=pow(x1.coords[i]-x2.coords[i],2);
+    }
+    return (distance);
   } else
     throw std::invalid_argument("Argument's dimension wrong");
 }
@@ -34,8 +42,7 @@ bool Point::operator==(const Point &obj) const {
   bool equal = true;
   if (obj.coords.size() == coords.size()) {
     for (int i = 0; i < coords.size() && equal; i++)
-      if (!approximatelyEqual(coords[i], obj.coords[i],
-                              std::numeric_limits<float>::epsilon()))
+      if (!approximatelyEqual(coords[i], obj.coords[i], std::numeric_limits<float>::epsilon()))
         equal = false;
     return equal;
   } else
@@ -43,10 +50,8 @@ bool Point::operator==(const Point &obj) const {
 }
 bool Point::operator!=(const Point &obj) const { return !operator==(obj); }
 
-bool Point::approximatelyEqual(float a, float b, float epsilon) const {
-  return fabs(a - b) <= epsilon;
-}
-// Add exception different size
+bool Point::approximatelyEqual(float a, float b, float epsilon) const { return fabs(a - b) <= epsilon; }
+
 Point Point::operator+(const Point &obj) const {
   Point new_point = Point(coords.size());
   if (obj.coords.size() == coords.size()) {
@@ -56,15 +61,16 @@ Point Point::operator+(const Point &obj) const {
   } else
     throw std::invalid_argument("Argument's dimension wrong");
 }
-// Add exception ...
+
 Point &Point::operator+=(const Point &obj) {
   if (obj.coords.size() == coords.size()) {
-    for (int i = 0; i < coords.size(); i++) coords[i] += obj.coords[i];
+    for (int i = 0; i < coords.size(); i++)
+      coords[i] += obj.coords[i];
     return *this;
   } else
     throw std::invalid_argument("Argument's dimension wrong");
 }
-// Add exception
+
 Point Point::operator-(const Point &obj) const {
   Point new_point = Point(coords.size());
   if (obj.coords.size() == coords.size()) {
@@ -74,10 +80,11 @@ Point Point::operator-(const Point &obj) const {
   } else
     throw std::invalid_argument("Argument's dimension wrong");
 }
-// Add exception ...
+
 Point &Point::operator-=(const Point &obj) {
   if (obj.coords.size() == coords.size()) {
-    for (int i = 0; i < coords.size(); i++) coords[i] -= obj.coords[i];
+    for (int i = 0; i < coords.size(); i++)
+      coords[i] -= obj.coords[i];
     return *this;
   } else
     throw std::invalid_argument("Argument's dimension wrong");
