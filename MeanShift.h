aab5a6c4837275a2ea2fe1f5be673b937406d589
VillifCoder559 <francesco.villi@gmail.com>
2022-11-24 16:42:06 +0100

    implement parallel centroids computation

--- MeanShift.h
+++ MeanShift.h
@@ -8,24 +8,22 @@
 #include <iostream>
 #include <map>
 #include <math.h>
-#include <vector>
 #include <omp.h>
+#include <vector>
+#include "ClusterCentroids.h"
 
 enum type_kernel { flat = 0, gaussian = 1 };
-
 class MeanShift {
 public:
   MeanShift(float bandwidth, type_kernel kernel, short int max_iterations = 50);
   ~MeanShift();
   std::vector<Point> fit(std::vector<Point> points);
-  // std::vector<Point> fit2(std::vector<Point> points);
   /*Find the closest cluster to point p*/
   unsigned short int predict(Point p);
   type_kernel get_type_kernel();
   /*id n-th corresponds to centroid in position n-th*/
   std::vector<unsigned short int> get_labels();
   float get_bandwidth();
-  // void compute_all_distances(std::vector<Point>& vector,int length,std::vector<float> &distances);
 
 private:
   std::vector<Point> centroids;
@@ -33,9 +31,7 @@ private:
   float bandwidth;
   Kernel *kernel;
   short int max_iterations;
-  Point compute_new_mean(std::vector<Point> &points, Point p);
-  // Point compute_new_mean(std::vector<Point> &points, float j, std::vector<float> &result);
-  std::vector<Point> compute_centroids(std::vector<Point> &points);
-  std::vector<float> find_nearest_centroid(Point &point, std::vector<Point> &centroids);
+  unsigned short int find_nearest_centroid(Point &point, std::vector<Point> &centroids);
+  std::vector<Point> compute_centroids_parallel(std::vector<Point> &points);
 };
 #endif
