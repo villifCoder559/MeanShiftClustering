a4412f0a43ae1e22cc3899dae9b30c98136f733d
VillifCoder559 <francesco.villi@gmail.com>
2022-11-24 16:23:01 +0100

    remove use of pow()

--- KernelGaussian.cpp
+++ KernelGaussian.cpp
@@ -3,6 +3,6 @@
 KernelGaussian::KernelGaussian() {}
 KernelGaussian::~KernelGaussian() {}
 float KernelGaussian::compute(float distance, float bandwith) {
-  float value = exp((-0.5 * pow(distance, 2) / (pow(bandwith, 2))));
-  return value > 0.00001 ? value : 0;
+  float value = exp(((distance*distance) / (bandwith*bandwith)));
+  return value > 0.000001 ? value : 0;
 }
